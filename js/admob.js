3a44b72fbbc1cf5117c9b481763ab835b73d5e27
akishore <abhikish2009@gmail.com>
2015-08-03 00:58:58 +0530

    Added admob js
    +Added admob.js as per admob pro plugin

---
+++ js/admob.js
@@ -0,0 +1,61 @@
+var admobid = {};
+
+if( /(android)/i.test(navigator.userAgent) ) { 
+	admobid = { // for Android
+		banner: 'ca-app-pub-6973712345433846/5193390011',
+		interstitial: 'ca-app-pub-6973712345433846/9623589610'
+	};
+} /* else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+	admobid = { // for iOS
+		banner: 'ca-app-pub-6869992474017983/4806197152',
+		interstitial: 'ca-app-pub-6869992474017983/7563979554'
+	};
+} else {
+	admobid = { // for Windows Phone
+		banner: 'ca-app-pub-6869992474017983/8878394753',
+		interstitial: 'ca-app-pub-6869992474017983/1355127956'
+	};
+} */
+
+/*
+if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+    document.addEventListener('deviceready', initApp, false);
+} else {
+    initApp();
+}
+
+function initApp() {
+    if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+
+    AdMob.createBanner( {
+        adId: admobid.banner, 
+        isTesting: true,
+        overlap: false, 
+        offsetTopBar: false, 
+        position: AdMob.AD_POSITION.BOTTOM_CENTER,
+        bgColor: 'black'
+    } );
+    
+    AdMob.prepareInterstitial({
+        adId: admobid.interstitial,
+        autoShow: true
+    });
+}
+*/
+//coppy
+	function initApp() {
+		if (AdMob) {
+			AdMob.createBanner({
+				adId : admobid.banner,
+				position : AdMob.AD_POSITION.BOTTOM_CENTER,
+				autoShow : true
+			});
+			AdMob.prepareInterstitial({
+				adId: admobid.interstitial,
+				autoShow: true
+			});
+			
+		}
+	}
+
+	document.addEventListener('deviceready', initApp, false);
